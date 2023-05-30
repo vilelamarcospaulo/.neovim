16cb229da23d5641ca46d5c54ce907e47c675979
vilelamarcospaulo <marcospaulo.vilelasantos@gmail.com>
2023-05-30 16:51:10 -0300

    using telescope to handle files

--- lua/vilelamarcospaulo/packer.lua
+++ lua/vilelamarcospaulo/packer.lua
@@ -3,4 +3,9 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
 end)
