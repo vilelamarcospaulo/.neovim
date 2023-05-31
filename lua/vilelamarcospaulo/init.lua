34d39699d8702e4b461ab3d59184eeee203c662f
marcos vilela <marcospaulo.vilelasantos@gmail.com>
2023-05-31 20:34:15 -0300

    configure tab to 2spc

--- lua/vilelamarcospaulo/init.lua
+++ lua/vilelamarcospaulo/init.lua
@@ -4,3 +4,9 @@ require('vilelamarcospaulo.remap')
 
 vim.wo.number = true
 -- vim.wo.relativenumber = true
+
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+
--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -32,9 +32,10 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', 'gr', "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  buf_set_keymap('n', 'ga', "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', vim.lsp.buf.rename, opts)
 
   vim.api.nvim_create_autocmd("CursorHold", {
     buffer = bufnr,
