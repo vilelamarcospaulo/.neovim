84cf7c9bd766fd26fdd280245a9cf88ffb335d95
vilelamarcospaulo <marcospaulo.vilelasantos@gmail.com>
2023-06-07 12:40:17 -0300

    configure spell checkers

--- lua/vilelamarcospaulo/init.lua
+++ lua/vilelamarcospaulo/init.lua
@@ -15,6 +15,9 @@ vim.o.smartindent = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 
+vim.opt.spelllang = "en_us"
+vim.opt.spell = true
+
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
