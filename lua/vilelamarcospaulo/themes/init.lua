8bed218b49f2122d9ba44f70a881c542eacae6a4
marcos vilela <marcospaulo.vilelasantos@gmail.com>
2023-08-27 17:29:24 -0300

    refact: move theme structure

---
+++ lua/vilelamarcospaulo/themes/init.lua
@@ -0,0 +1,6 @@
+require('vilelamarcospaulo.themes.catppuccin')
+require('vilelamarcospaulo.themes.nightfox')
+require('vilelamarcospaulo.themes.onedark')
+
+vim.cmd 'colorscheme onedark'
+vim.cmd 'set termguicolors'
