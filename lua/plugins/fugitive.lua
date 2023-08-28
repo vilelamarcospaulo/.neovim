5cd875ab19962d9b463af51b56f7f7b37db2bc46
vilelamarcospaulo <marcospaulo.vilelasantos@gmail.com>
2023-08-28 10:27:20 -0300

    fix: correctly install fugitive deps

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,29 +1,25 @@
 return {
   'tpope/vim-fugitive',
-  depends = {
+  dependencies = {
     'tpope/vim-rhubarb',
     'airblade/vim-gitgutter',
+    'braxtons12/blame_line.nvim',
   },
   config = function()
     vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
     vim.keymap.set("n", "<leader>gb", vim.cmd.BlameLineToggle)
     vim.keymap.set("n", "<leader>gh", "<cmd>:0Gclog<CR>")
 
+    local blame = require("blame_line")
+    blame.setup {
+      prefix = " ",
+      template = "<author> • <author-time> • <summary> <commit-short>",
+      date = {
+        relative = false,
+        format = "%m-%d-%y",
+      },
+    }
 
-    vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-    vim.keymap.set("n", "<leader>gb", vim.cmd.BlameLineToggle)
-    vim.keymap.set("n", "<leader>gh", "<cmd>:0Gclog<CR>")
-
-    -- local blame = require("blame_line")
-    -- blame.setup {
-    --   prefix = " ",
-    --   template = "<author> • <author-time> • <summary> <commit-short>",
-    --   date = {
-    --     relative = false,
-    --     format = "%m-%d-%y",
-    --   },
-    -- }
-    --
-    -- blame.disable()
+    blame.disable()
   end
 }
