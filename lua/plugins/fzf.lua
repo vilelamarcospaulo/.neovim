314a467db7610c3a24837f7c32c5512b93b0ebf7
vilelamarcospaulo <marcospaulo.vilelasantos@gmail.com>
2024-12-04 12:21:56 -0300

    wip: Fzf borderless preview

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -10,14 +10,20 @@ return {
         formatter    = "path.filename_first",
         path_shorten = false,
       },
-      files    = {
+      files = {
         cwd_prompt             = true,
         cwd_prompt_shorten_len = 32, -- shorten prompt beyond this length
         cwd_prompt_shorten_val = 1,  -- shortened path parts length
       },
-      keymap   = {
+      keymap = {
         fzf = {
           ["ctrl-q"] = "select-all+accept",
+        },
+      },
+      winopts = {
+        border = 'none',
+        preview = {
+          title_pos = 'left',
         },
       },
     })
--- lua/plugins/nightly.lua
+++ lua/plugins/nightly.lua
@@ -1,3 +1,27 @@
+local white      = "#FFFFFF"
+local fg         = "#AEAEAE"
+local light_bg   = "#202a2e"
+local darker_bg  = "#101518"
+local limit_bg   = "#2e3d42"
+local background = "#141B1E"
+
 return {
   "vilelamarcospaulo/nightly.nvim",
+  config = function()
+    require("nightly").setup({
+      transparent = false,
+      styles = {
+        comments  = { italic = true },
+        functions = { italic = false },
+        variables = { italic = false },
+        keywords  = { italic = false },
+      },
+      highlights = {
+        FzfLuaPreviewTitle = { fg = fg, bg = darker_bg },
+        FzfLuaPreviewBorder = { fg = fg, bg = darker_bg },
+        FzfLuaFzfScrollbar = { fg = background, bg = background },
+        -- FzfLuaFzfGutter = { fg = background, bg = background },
+      },
+    })
+  end,
 }
