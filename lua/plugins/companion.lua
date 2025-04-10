7358c8e8bf3b539825e293d44b0c28ff79d2c2b9
marcos vilela <marcospaulo.vilelasantos@gmail.com>
2025-04-10 20:58:02 -0300

    use claude for  copilot

--- lua/plugins/companion.lua
+++ lua/plugins/companion.lua
@@ -8,7 +8,19 @@ return {
   },
   config = function()
     require("plugins.codecompanion.notification"):init()
-    require("codecompanion").setup {}
+    require("codecompanion").setup {
+      adapters = {
+        copilot = function()
+          return require("codecompanion.adapters").extend("copilot", {
+            schema = {
+              model = {
+                default = "claude-3.5-sonnet",
+              },
+            },
+          })
+        end,
+      }
+    }
 
     vim.keymap.set({ "n", "v" }, "<leader>cc", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
     vim.keymap.set({ 'n', 'v' }, '<leader>aa', ':CodeCompanion #buffer ', { noremap = true, silent = false })
